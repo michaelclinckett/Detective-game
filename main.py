ac58a10398dedfeeba88383a36314d072b1f2c81
Michael Clinckett <18381@jhc.school.nz>
2021-03-11 00:22:55 +0000

    Finished level 1

--- main.py
+++ main.py
@@ -17,14 +17,33 @@ print("You are a detective\n")
 time.sleep(2)
 print("Solve the case\n")
 time.sleep(2)
-print("Don't catch the wrong guy")
+print("Don't catch the wrong guy\n")
 time.sleep(2)
-input("Press enter to start")
+input("Press enter to start level 1")
 time.sleep(2)
-print("A murder was carried out on the 26th of October, 1976\n")
+print("\nA murder was carried out on the 26th of October, 1976\n")
 time.sleep(2)
 print("4 suspects were caught and you are here to question them\n")
 time.sleep(2)
-print("ask questions")
+print("ask questions to find out who the murder is\n")
+time.sleep(2)
+print("Suspect 1 said he was at Kmart when the murder was commited\n")
+time.sleep(2)
+print("Suspect 2 said he saw a movie at the time of the murder\n")
+time.sleep(2)
+print("Suspect 3 said he was at home when victim was murdered\n")
+time.sleep(2)
+print("Suspect 4 said he was at home sleeping\n")
+time.sleep(2)
+print("\nThe murder was commited at 1 am in the morning\n")
+try:
+  murder=float(input("Who is the murder? (number of suspect)>>>"))
+  if murder == 1:
+    print("\nYou found the correct murder. ")
+  else:
+    print("\nYou got the wrong man. You are fired from your job and are fined $10000. Restart to try again")
+except ValueError:
+  quit("\nYou got fired for invalid input of suspect. Restart to try again")
+
 
 
