14fd248fef1b592ddd433804d4f9811081ccd041
Michael Clinckett <18381@jhc.school.nz>
2021-03-14 20:08:13 +0000

    Bug??

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 
-str = "Messi is the best soccer player"
->>> "soccer" in str
-True
->>> "football" in str
-False
+
+
 
 
 
@@ -59,3 +56,41 @@ input("Press enter to start level 2")
 time.sleep(2)
 print("Use questions to find out who the thief is.")
 
+while True:
+  time.sleep(2)
+  sentance=(input("\nAsk a question or put in a number of the suspect who commited the crime>>>"))
+  if "where" and "you" in sentance:
+    print("Suspect 1 was at a friends house")
+    print("Suspect 2 was sleeping")
+    print("Suspect 3 was eating dinner with his family")
+    print("Suspect 4 was watching tv at home")
+  elif "time" in sentance:
+    print("The theif struck at 9:00pm on Wednesday, 2009")
+  elif "alibi" in sentance:
+    print("Suspect 1's friend says he was with him the whole time")
+    print("Suspect 2's wife said that he went to bed at around 8:30pm")
+    print("Suspect 3's family said he was eating dinner from 8:30 to 9:30 ")
+    print("Suspect 4 does not have an alibi")
+  elif "where" and "crime" in sentance:
+    print("The crime was commited at the house next door to Pak'n'save")
+  elif "what" and "doing" and "were" in sentance:
+    print("Suspect 1 was at Pak'n'save with his friend")
+    print("Suspect 2 was sleeping")
+    print("Suspect 3 was eating pizza t home with his family")
+    print("Suspect 4 was at home watching Thomas the tank engine on tv")
+  elif "tv" in sentance:
+    print("From 8pm to 9pm was Thomas the tank engine on TV")
+  
+  
+  elif "friend" and "what" in sentance:
+    print("Suspect 1 and friend walked past the robbed house and went up the drive of the house according to a passbyer. He did not see them come out.")
+  elif "1" or "suspect 1" in sentance:
+    print("Great job, you got the man. He and his friend commited the crime togther.")
+    break
+  elif "2" or "3" or "4" in sentance:
+    print("You are fired from your job. Restart to try again")
+    quit()
+  else: 
+    print("This qusetion cannot be answered")
+print("entering level 3 (coming soon)")
+   
