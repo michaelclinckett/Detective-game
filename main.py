381645cd52ced8609a53647fb8fff90c8c82ccb4
Michael Clinckett <18381@jhc.school.nz>
2021-03-15 00:20:33 +0000

    Attempt 3 at fixing incorrect statement

--- main.py
+++ main.py
@@ -11,7 +11,42 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 
 
+print("You are a detective\n")
+time.sleep(2)
+print("Solve the case\n")
+time.sleep(2)
+print("Don't catch the wrong guy\n")
+time.sleep(2)
+input("Press enter to start level 1")
+time.sleep(2)
+print("\nA murder was carried out on the 26th of October, 1976\n")
+time.sleep(2)
+print("4 suspects were caught and you are here to find out who commited the murder\n")
 
+time.sleep(2)
+print("Suspect 1 said he was at Kmart at the time of the murder\n")
+time.sleep(2)
+print("Suspect 2 said he saw a movie at the time of the murder\n")
+time.sleep(2)
+print("Suspect 3 said he was at home when victim was murdered\n")
+time.sleep(2)
+print("Suspect 4 said he was at home sleeping\n")
+time.sleep(2)
+print("The murder was commited at 1 am in the morning\n")
+try:
+  murder=float(input("Who is the murder? (number of suspect)>>>"))
+  if murder != 1:
+    print("\nYou got the wrong man. You are fired from your job and are fined $10000. Restart to try again")
+    quit()
+  else:
+    print("\nYou found the correct murderer. Reasons are Kmart is not open at 1am and Kmart did not exist in 1976.")
+    time.sleep(2)
+    print("Entering level 2")
+    time.sleep(4)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+except ValueError:
+  quit("\nYou got fired for invalid input of suspect. Restart to try again")
 
 
 
@@ -44,7 +79,7 @@ while True:
     print("Suspect 1 and friend walked past the robbed house and went up the drive of the house according to a passbyer. He did not see them come out.")
   if sentance != "1" or "2" or "3" or "4":
     print("This question can not be answered")
-  if sentance != "1":
+  if sentance == "2" or "3" or"4":
     print("You are fired from your job. Restart to try again")
     quit()
   if sentance == "1":
