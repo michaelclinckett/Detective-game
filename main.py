4b8f2f3d4aa47638a2cf9a3fb26a9354db03409e
Michael Clinckett <18381@jhc.school.nz>
2021-03-15 20:24:03 +0000

    Sped up the game waiting time

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 
 print("You are a detective\n")
-time.sleep(2)
+time.sleep(1)
 print("Solve the case\n")
-time.sleep(2)
+time.sleep(1)
 print("Don't catch the wrong guy\n")
-time.sleep(2)
+time.sleep(1)
 input("Press enter to start level 1")
-time.sleep(2)
+time.sleep(1)
 print("\nA murder was carried out on the 26th of October, 1976\n")
-time.sleep(2)
+time.sleep(1)
 print("4 suspects were caught and you are here to find out who commited the murder\n")
 
-time.sleep(2)
+time.sleep(1)
 print("Suspect 1 said he was at Kmart at the time of the murder\n")
-time.sleep(2)
+time.sleep(1)
 print("Suspect 2 said he saw a movie at the time of the murder\n")
-time.sleep(2)
+time.sleep(1)
 print("Suspect 3 said he was at home when victim was murdered\n")
-time.sleep(2)
+time.sleep(1)
 print("Suspect 4 said he was at home sleeping\n")
-time.sleep(2)
+time.sleep(1)
 print("The murder was commited at 1 am in the morning\n")
 try:
   murder=float(input("Who is the murder? (number of suspect)>>>"))
@@ -40,7 +40,7 @@ try:
     quit()
   else:
     print("\nYou found the correct murderer. Reasons are Kmart is not open at 1am and Kmart did not exist in 1976.")
-    time.sleep(2)
+    time.sleep(1)
     print("Entering level 2")
     time.sleep(4)
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -51,13 +51,13 @@ except ValueError:
 
 
   
-time.sleep(2)
+time.sleep(1)
 input("Press enter to start level 2")
-time.sleep(2)
+time.sleep(1)
 print("Use questions to find out who the thief is.")
 
 while True:
-  time.sleep(2)
+  time.sleep(1)
   sentance=(input("\nAsk a question or put in a number of the suspect who commited the crime>>> "))
   if "where" and "you" in sentance:
     print("Suspect 1 was at a friends house")
