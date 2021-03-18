37d29667ed88afea0d1c86418d31af9f10d2bd97
Michael Clinckett <18381@jhc.school.nz>
2021-03-18 00:00:46 +0000

    Started level 3

--- main.py
+++ main.py
@@ -54,8 +54,51 @@ except ValueError:
 time.sleep(1)
 input("Press enter to start level 2")
 time.sleep(1)
-print("Use questions to find out who the thief is.")
+print("A computer, tv and ps5 were stolen. ")
+time.sleep(1)
+print("Find out who the thief is and ask the suspects and officers at the police station to find the thief by asking questions")
 
+
+while True:
+  time.sleep(1)
+  sentence=(input("\nAsk a question or put in a number of the suspect who commited the crime\n>>> "))
+  if "where" in sentence and "you" in sentence:
+    print("Suspect 1 was at a friends house")
+    print("Suspect 2 was sleeping")
+    print("Suspect 3 was eating dinner with his family")
+    print("Suspect 4 was watching tv at home")
+  elif "time" in sentence:
+    print("The theif struck at 9:00pm on Wednesday, 2009")
+  elif "alibi" in sentence:
+    print("Suspect 1's friend says he was with him the whole time. They were at Pak'n'save till 10:00pm")
+    print("Suspect 2's wife said that he went to bed at around 8:30pm")
+    print("Suspect 3's family said he was eating pizza with them from 8:30 to 9:30 ")
+    print("Suspect 4 does not have an alibi but aid he was watching Thomas the tank engine on TV")
+  elif "where" in sentence and "crime" in sentence:
+    print("The crime was commited at the house next door to Pak'n'save")
+  elif "tv" in sentence:
+    print("From 8pm to 9pm was Thomas the tank engine on TV")
+  elif "friend" in sentence and "what" in sentence:
+    print("Suspect 1 and friend walked past the robbed house and went up the drive of the house according to a passbyer. He did not see them come out.")
+  elif sentence == "2" or sentence == "3" or sentence == "4":
+    print("You are fired from your job. Restart to try again")
+    quit()
+  elif sentence == "1":
+    print("Great job, you got the man. He and his friend commited the crime togther.")
+    time.sleep(1)
+    print("entering level 3")
+    time.sleep(4)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    break
+  else: 
+     print("This question can not be answered")
+
+input("Press enter to start level 3")
+print("A kidnapping has occured.")
+time.sleep(1)
+print("There are 4 suspects and each one has a personality")
+time.sleep(1)
+print("Ask questions to find the kidnapper. Remember, each suspect has a personality so the suspects may not answer your questions straight away ")
 while True:
   time.sleep(1)
   sentence=(input("\nAsk a question or put in a number of the suspect who commited the crime\n>>> "))
