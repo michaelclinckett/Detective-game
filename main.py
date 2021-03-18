21d2f92ab5d1f957851a962ca78a3364b1c9f20d
Michael Clinckett <18381@jhc.school.nz>
2021-03-18 20:32:24 +0000

    Finish level 3

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ while True:
     print("Suspect 1's friend says he was with him the whole time. They were at Pak'n'save till 10:00pm")
     print("Suspect 2's wife said that he went to bed at around 8:30pm")
     print("Suspect 3's family said he was eating pizza with them from 8:30 to 9:30 ")
-    print("Suspect 4 does not have an alibi but aid he was watching Thomas the tank engine on TV")
+    print("Suspect 4 does not have an alibi but said he was watching Thomas the tank engine on TV")
   elif "where" in sentence and "crime" in sentence:
     print("The crime was commited at the house next door to Pak'n'save")
   elif "tv" in sentence:
@@ -92,43 +92,54 @@ while True:
     break
   else: 
      print("This question can not be answered")
-
+second_time=0
 input("Press enter to start level 3")
 print("A kidnapping has occured.")
 time.sleep(1)
 print("There are 4 suspects and each one has a personality")
 time.sleep(1)
-print("Ask questions to find the kidnapper. Remember, each suspect has a personality so the suspects may not answer your questions straight away ")
+print("Ask questions to find the kidnapper. Remember, each suspect has a personality so the suspects may not answer your questions straight away and truthfully")
 while True:
   time.sleep(1)
-  sentence=(input("\nAsk a question or put in a number of the suspect who commited the crime\n>>> "))
+  sentence=(input("\nAsk a question or put in a number of the suspect who commited kidnapping\n>>> "))
   if "where" in sentence and "you" in sentence:
-    print("Suspect 1 was at a friends house")
-    print("Suspect 2 was sleeping")
-    print("Suspect 3 was eating dinner with his family")
-    print("Suspect 4 was watching tv at home")
+    print("Suspect 1 refuses to answer")
+    print("Suspect 2 was at work")
+    print("Suspect 3 was was peforming at a show")
+    print("Suspect 4 was watching suspect 3 on tv")
   elif "time" in sentence:
-    print("The theif struck at 9:00pm on Wednesday, 2009")
-  elif "alibi" in sentence:
-    print("Suspect 1's friend says he was with him the whole time. They were at Pak'n'save till 10:00pm")
-    print("Suspect 2's wife said that he went to bed at around 8:30pm")
-    print("Suspect 3's family said he was eating pizza with them from 8:30 to 9:30 ")
-    print("Suspect 4 does not have an alibi but aid he was watching Thomas the tank engine on TV")
+    print("The kidnapping happened at 6:00pm on Thursday, 18th of March, 2021")
+  elif "alibi" in sentence and second_time == 0:
+    second_time == 1
+    print("Suspect 1 refuses to answer")
+    print("Suspect 2 co-worker says he was at work till 8:00pm")
+    print("Suspect 3 had over 1000 alibis ")
+    print("Suspect 4 does not have an alibi but says he was watching suspect 3 on tv till the show ended")
   elif "where" in sentence and "crime" in sentence:
-    print("The crime was commited at the house next door to Pak'n'save")
-  elif "tv" in sentence:
-    print("From 8pm to 9pm was Thomas the tank engine on TV")
-  elif "friend" in sentence and "what" in sentence:
-    print("Suspect 1 and friend walked past the robbed house and went up the drive of the house according to a passbyer. He did not see them come out.")
-  elif sentence == "2" or sentence == "3" or sentence == "4":
+    print("The kidnapping happened at house number 5 on third avenue")
+  elif "tv" in sentence or "show" in sentence or "performence" in sentence:
+    print("From 6pm to 9pm suspect 3 performed his 1 man women interpretation. This was broadcasted on tv")
+  elif "alibi" in sentence and second_time == 1:
+    print("Suspect 1 said he has no alibi")
+    print("Suspect 2 co-worker says he was at work till 8:00pm")
+    print("A fan at the show got a signature of suspect 3.")
+    print("Suspect 4 does not have an alibi but says he was watching suspect 3 on tv till the show ended")
+    second_time == 0
+  elif "signature" in sentence:
+    print("suspect 1 says he doesn't have a signature")
+    print("Suspect 2 has a signature but can be confirmed that that is his signature")
+    print("Suspect 3's signature does not match the one taken at the show")
+    print("Suspect 4's signature can not be confirmed")
+  elif sentence == "2" or sentence == "1" or sentence == "4":
     print("You are fired from your job. Restart to try again")
     quit()
-  elif sentence == "1":
-    print("Great job, you got the man. He and his friend commited the crime togther.")
+  elif sentence == "3":
+    print("Great job, you got the man. He switched places with his twin sister at the show and commited the kidnapping.")
     break
   else: 
      print("This question can not be answered")
-print("entering level 3 (coming soon)")
+print("CONGRATS!!! You finished the detective game. You are now a qualified detective")
+
 
 
 
